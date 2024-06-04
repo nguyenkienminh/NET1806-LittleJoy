9f8f2d9832a3a3395542f0fbeed7e0d5821b11d6
lion3993vn <lion3993vn@gmail.com>
2024-06-04 15:23:12 +0700

    add: blog feature

---
+++ NET1806_LittleJoyStore/NET1806_LittleJoy.API/Controllers/BlogController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NET1806_LittleJoy.Repository.Commons;
+using NET1806_LittleJoy.Service.Services;
+using NET1806_LittleJoy.Service.Services.Interface;
+
+namespace NET1806_LittleJoy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogController(IBlogService blogService) 
+        {
+            _blogService = blogService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetListBlog([FromQuery] PaginationParameter paginationParameter)
+        {
+            //try
+            //{
+            //    var result;
+            //}catch (Exception ex)
+            //{
+
+            //}
+            return Ok();
+        }
+    }
+}
---
+++ NET1806_LittleJoyStore/NET1806_LittleJoy.Service/Services/BlogService.cs
@@ -0,0 +1,23 @@
+﻿using NET1806_LittleJoy.Repository.Repositories;
+using NET1806_LittleJoy.Repository.Repositories.Interface;
+using NET1806_LittleJoy.Service.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET1806_LittleJoy.Service.Services
+{
+    public class BlogService : IBlogService
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public BlogService(IBlogRepository blogRepository) 
+        {
+            _blogRepository = blogRepository;
+        }
+
+
+    }
+}
