0dd769cd801696a829dcd192abb46955ef3d98dd
lion3993vn <lion3993vn@gmail.com>
2024-06-07 21:40:39 +0700

    authorize blog

--- NET1806_LittleJoyStore/NET1806_LittleJoy.API/Controllers/BlogController.cs
+++ NET1806_LittleJoyStore/NET1806_LittleJoy.API/Controllers/BlogController.cs
@@ -90,6 +90,7 @@ namespace NET1806_LittleJoy.API.Controllers
         }
 
         [HttpPost]
+        [Authorize(Roles = "STAFF, ADMIN")]
         public async Task<IActionResult> CreateNewBlog(BlogRequestModel blog)
         {
             try
@@ -126,6 +127,7 @@ namespace NET1806_LittleJoy.API.Controllers
         }
 
         [HttpPut]
+        [Authorize(Roles = "STAFF, ADMIN")]
         public async Task<IActionResult> UpdateBlog(BlogRequestUpdateModel blog)
         {
             try
@@ -164,6 +166,7 @@ namespace NET1806_LittleJoy.API.Controllers
         }
 
         [HttpDelete]
+        [Authorize(Roles = "STAFF, ADMIN")]
         public async Task<IActionResult> RemoveBlog(int id)
         {
             try
@@ -194,6 +197,7 @@ namespace NET1806_LittleJoy.API.Controllers
         }
 
         [HttpGet("filter")]
+        [Authorize(Roles = "STAFF, ADMIN")]
         public async Task<IActionResult> GetListBlogFilter([FromQuery] BlogFilterModel filter, [FromQuery] PaginationParameter paging)
         {
             try
@@ -231,7 +235,6 @@ namespace NET1806_LittleJoy.API.Controllers
                 };
                 return BadRequest(responseModel);
             }
-            return Ok();
         }
     }
 }
