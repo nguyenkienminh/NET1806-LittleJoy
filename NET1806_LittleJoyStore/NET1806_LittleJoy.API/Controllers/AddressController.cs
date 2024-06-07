2d8e57b321c559af70939a5a55d1e1b8062b2e3c
nguyenkienminh <kienminh04122003@gmail.com>
2024-06-07 10:35:20 +0700

    Add - Address CR

---
+++ NET1806_LittleJoyStore/NET1806_LittleJoy.API/Controllers/AddressController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NET1806_LittleJoy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+    }
+}
