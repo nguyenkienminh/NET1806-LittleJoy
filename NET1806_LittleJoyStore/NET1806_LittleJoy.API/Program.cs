82f8a3d7ec4fbf9ca2ca96e48e3f6b4f17ed5138
lion3993vn <lion3993vn@gmail.com>
2024-06-11 19:13:27 +0700

    fix: program

--- NET1806_LittleJoyStore/NET1806_LittleJoy.API/Program.cs
+++ NET1806_LittleJoyStore/NET1806_LittleJoy.API/Program.cs
@@ -135,16 +135,12 @@ namespace NET1806_LittleJoy.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
